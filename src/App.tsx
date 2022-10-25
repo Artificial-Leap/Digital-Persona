78a0d32bb2ee487704588d49ca4336c413b5a096
alextitonis <alexandrostitonis@gmail.com>
2022-10-25 14:54:21 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { CharacterEditor } from "avatarcreator/src/index";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <CharacterEditor />
+    </div>
+  );
+}
+
+export default App;
